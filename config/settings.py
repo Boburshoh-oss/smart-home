8206b807b5c19b3ae050e8fb273451f33e8e9f22
Boburshoh-oss <BoburbekBotirov868@gmail.com>
2022-08-02 14:25:28 +0500

    static

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,7 @@ STATIC_URL = '/static/'
 # if DEBUG:
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 # else:
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'home/ubuntu/smart-home/static/'
 
 STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 
